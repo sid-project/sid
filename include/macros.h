2e90c6f5af0a3a9149f3e44f7b7038d11bb9b12a
Peter Rajnoha <prajnoha@redhat.com>
2019-01-29 15:33:23 +0100

    global: add missing macros.h
    This should have been added together with commit
    63d08774b03caae5efc6504c521b150e1d0c86b1.

---
+++ include/macros.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of SID.
+ *
+ * Copyright (C) 2019 Red Hat, Inc. All rights reserved.
+ *
+ * SID is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * SID is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SID.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SID_MACROS_H
+#define _SID_MACROS_H
+
+#define SYSTEM_DEV_PATH           "/dev"
+#define SYSTEM_SYSFS_PATH         "/sys"
+#define SYSTEM_PROC_PATH          "/proc"
+#define SYSTEM_PROC_DEVICES_PATH  SYSTEM_PROC_PATH "/devices"
+
+#endif
