8a57d3e531dce3ae66d03939c51e2f4b2fa391dc
Peter Rajnoha <prajnoha@redhat.com>
2020-03-04 12:50:27 +0100

    cleanup: remove declaration for undefined sid_resource_type_kv_store_hash

--- src/resource/kv-store.c
+++ src/resource/kv-store.c
@@ -33,8 +33,6 @@ typedef enum {
 	KV_STORE_VALUE_INT_ALLOC = UINT32_C(0x00000001),
 } kv_store_value_int_flags_t;
 
-const sid_resource_type_t sid_resource_type_kv_store_hash;
-
 struct kv_store {
 	struct hash_table *ht;
 };
