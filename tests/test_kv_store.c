c4663910572cd9826b35efaba9889366f9c6603b
Benjamin Marzinski <bmarzins@redhat.com>
2020-04-20 16:42:52 -0500

    tests: update test_kv_store to check fix details

--- tests/test_kv_store.c
+++ tests/test_kv_store.c
@@ -32,11 +32,28 @@ static void test_type_H(void **state)
 		{"value", sizeof("value")}
 	};
 	size_t size = sizeof(test_iov)/sizeof(test_iov[0]);
-	struct kv_store_value *value = _create_kv_store_value(test_iov, size,
-	                                                      KV_STORE_VALUE_REF | KV_STORE_VALUE_VECTOR,
-	                                                      KV_STORE_VALUE_OP_MERGE);
+	struct iovec old_iov[size];
+	struct kv_store_value *value;
+	int i;
+
+	memcpy(old_iov, test_iov, sizeof(old_iov));
+	value = _create_kv_store_value(test_iov, size,
+	                               KV_STORE_VALUE_REF | KV_STORE_VALUE_VECTOR,
+	                               KV_STORE_VALUE_OP_MERGE);
 	assert_ptr_not_equal(value, NULL);
+	assert_ptr_equal(value->data_p, test_iov);
+	assert_int_equal(value->size, size);
+	for (i = 0; i < size; i++)
+		assert_ptr_not_equal(test_iov[i].iov_base,
+		                     old_iov[i].iov_base);
+	assert_int_equal(value->int_flags, KV_STORE_VALUE_INT_ALLOC);
+	assert_int_equal(value->ext_flags,
+	                 KV_STORE_VALUE_REF | KV_STORE_VALUE_VECTOR);
 	_destroy_kv_store_value(value);
+	for (i = 0; i < size; i++) {
+		assert_ptr_equal(test_iov[i].iov_base, NULL);
+		assert_int_equal(test_iov[i].iov_len, 0);
+	}
 }
 
 int main(void)
