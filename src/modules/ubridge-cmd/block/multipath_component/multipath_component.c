2e28703f9eaa9f88cc8cb482e5fdbb03de2f3252
Benjamin Marzinski <bmarzins@redhat.com>
2020-05-22 16:13:46 -0500

    modules: ubridge-cmd: block: update multipath module again
    make multipath_component module set the DM_MULTIPATH_DEVICE_PATH
    keyword. Also, the libmpathvalid library interface has changed again.
    Updated the multipath module to use the new API.

--- src/modules/ubridge-cmd/block/multipath_component/multipath_component.c
+++ src/modules/ubridge-cmd/block/multipath_component/multipath_component.c
@@ -21,14 +21,41 @@
 #include "resource/ubridge-cmd-module.h"
 
 #include <limits.h>
+#include <libudev.h>
 #include <mpath_valid.h>
 #include <stdio.h>
 
 #define ID "multipath_component"
+#define KEY "DM_MULTIPATH_DEVICE_PATH"
+struct udev *udev;
+int logsink = -1;
+
+struct config *get_multipath_config(void)
+{
+	return mpathvalid_conf;
+}
+
+void put_multipath_config(__attribute__((unused))void *conf)
+{
+	/* Noop */
+}
 
 static int _multipath_component_init(struct sid_module *module, struct sid_ubridge_cmd_mod_context *cmd_mod)
 {
 	log_debug(ID, "init");
+	/* TODO - set up dm/udev logging */
+	udev = udev_new();
+	if (!udev) {
+		log_error(ID, "failed to allocate udev context");
+		return -1;
+	}
+	if (sid_ubridge_cmd_mod_reserve_kv(module, cmd_mod, KV_NS_UDEV,
+	                                   KEY) < 0) {
+		log_error(ID, "Failed to reserve multipath udev key %s", KEY);
+		udev_unref(udev);
+		udev = NULL;
+		return -1;
+	}
 	return 0;
 }
 SID_UBRIDGE_CMD_MOD_INIT(_multipath_component_init)
@@ -36,6 +63,9 @@ SID_UBRIDGE_CMD_MOD_INIT(_multipath_component_init)
 static int _multipath_component_exit(struct sid_module *module, struct sid_ubridge_cmd_mod_context *cmd_mod)
 {
 	log_debug(ID, "exit");
+	// Do we need to unreserve the key here?
+	udev_unref(udev);
+	udev = NULL;
 	return 0;
 }
 SID_UBRIDGE_CMD_MOD_EXIT(_multipath_component_exit)
@@ -49,14 +79,27 @@ SID_UBRIDGE_CMD_MOD_RELOAD(_multipath_component_reload)
 
 static int _multipath_component_scan_pre(struct sid_module *module, struct sid_ubridge_cmd_context *cmd)
 {
+	int r;
 	log_debug(ID, "scan-pre");
 
-	if (mpath_is_path(sid_ubridge_cmd_dev_get_name(cmd), MPATH_DEFAULT,
-	                  NULL, NULL, 0)) {
-		// mark with appropriate key=value pair
+	if (mpathvalid_init(-1) < 0) {
+		log_error(ID, "failed to initialize mpathvalid");
+		return -1;
 	}
-
-	return 0;
+	// currently treats MPATH_SMART like MPATH_STRICT
+	r = mpathvalid_is_path(sid_ubridge_cmd_dev_get_name(cmd), MPATH_DEFAULT,
+	                       NULL, NULL, 0);
+	log_debug(ID, "mpathvalid_is_path returned %d", r);
+	if (r == MPATH_IS_VALID || r == MPATH_IS_VALID_NO_CHECK) {
+		sid_ubridge_cmd_set_kv(cmd, KV_NS_UDEV, KEY, "1", 2,
+		                       KV_MOD_PROTECTED);
+		// mark with appropriate key=value pair
+	} else if (r != MPATH_IS_ERROR) {
+		sid_ubridge_cmd_set_kv(cmd, KV_NS_UDEV, KEY, "0", 2,
+		                       KV_MOD_PROTECTED);
+	}
+	mpathvalid_exit();
+	return (r != MPATH_IS_ERROR);
 }
 SID_UBRIDGE_CMD_SCAN_PRE(_multipath_component_scan_pre)
 
