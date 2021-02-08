846ddf290a0aea4ef3bdcc69f2a8f640b8f6d99c
Peter Rajnoha <prajnoha@redhat.com>
2021-02-08 16:55:23 +0100

    tests: sync usid test with recent buffer changes

--- tests/test_usid.c
+++ tests/test_usid.c
@@ -34,7 +34,14 @@ static void test_checkpoint_env(void **state)
 		.argv = test_argv
 	};
 
-	buf = buffer_create(BUFFER_TYPE_LINEAR, BUFFER_MODE_SIZE_PREFIX, 0, 1, 0, NULL);
+	buf = buffer_create(&((struct buffer_spec) {.backend = BUFFER_BACKEND_MALLOC,
+						    .type = BUFFER_TYPE_LINEAR,
+						    .mode = BUFFER_MODE_SIZE_PREFIX}),
+			    &((struct buffer_init){.size = 0,
+						   .alloc_step = 1,
+						   .limit = 0}),
+			    NULL);
+
 	assert_non_null(buf);
 	will_return(__wrap_getenv, "8");
 	will_return(__wrap_getenv, "0");
