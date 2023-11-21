0a7d0d68549f14dd82cb5caf0b93509486aa1333
Peter Rajnoha <prajnoha@redhat.com>
2023-11-21 15:42:58 +0100

    ucmd: module: dummy_block: replace struct module* type with sid_resource_t* in module callbacks and ucmd API calls

--- src/modules/ucmd/block/dummy_block/dummy_block.c
+++ src/modules/ucmd/block/dummy_block/dummy_block.c
@@ -24,91 +24,91 @@
 
 SID_UCMD_MOD_PRIO(1)
 
-static int _dummy_block_init(struct module *module, struct sid_ucmd_common_ctx *ucmd_common_ctx)
+static int _dummy_block_init(sid_resource_t *mod_res, struct sid_ucmd_common_ctx *ucmd_common_ctx)
 {
 	log_debug(MID, "init");
 	return 0;
 }
 SID_UCMD_MOD_INIT(_dummy_block_init)
 
-static int _dummy_block_exit(struct module *module, struct sid_ucmd_common_ctx *ucmd_common_ctx)
+static int _dummy_block_exit(sid_resource_t *mod_res, struct sid_ucmd_common_ctx *ucmd_common_ctx)
 {
 	log_debug(MID, "exit");
 	return 0;
 }
 SID_UCMD_MOD_EXIT(_dummy_block_exit)
 
-static int _dummy_block_reset(struct module *module, struct sid_ucmd_common_ctx *ucmd_common_ctx)
+static int _dummy_block_reset(sid_resource_t *mod_res, struct sid_ucmd_common_ctx *ucmd_common_ctx)
 {
 	log_debug(MID, "reset");
 	return 0;
 }
 SID_UCMD_MOD_RESET(_dummy_block_reset)
 
-static int _dummy_block_ident(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_ident(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "ident");
 	return 0;
 }
 SID_UCMD_IDENT(_dummy_block_ident)
 
-static int _dummy_block_scan_pre(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_scan_pre(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "scan-pre");
 	return 0;
 }
 SID_UCMD_SCAN_PRE(_dummy_block_scan_pre)
 
-static int _dummy_block_scan_current(struct module *module, struct sid_ucmd_ctx *ucmd)
+static int _dummy_block_scan_current(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd)
 {
 	log_debug(MID, "scan-current");
 	return 0;
 }
 SID_UCMD_SCAN_CURRENT(_dummy_block_scan_current)
 
-static int _dummy_block_scan_next(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_scan_next(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "scan-next");
 	return 0;
 }
 SID_UCMD_SCAN_NEXT(_dummy_block_scan_next)
 
-static int _dummy_block_scan_post_current(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_scan_post_current(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "scan-post-current");
 	return 0;
 }
 SID_UCMD_SCAN_POST_CURRENT(_dummy_block_scan_post_current)
 
-static int _dummy_block_scan_post_next(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_scan_post_next(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "scan-post-next");
 	return 0;
 }
 SID_UCMD_SCAN_POST_NEXT(_dummy_block_scan_post_next)
 
-static int _dummy_block_trigger_action_current(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_trigger_action_current(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "trigger-action-current");
 	return 0;
 }
 SID_UCMD_TRIGGER_ACTION_CURRENT(_dummy_block_trigger_action_current)
 
-static int _dummy_block_trigger_action_next(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_trigger_action_next(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "trigger-action-next");
 	return 0;
 }
 SID_UCMD_TRIGGER_ACTION_NEXT(_dummy_block_trigger_action_next)
 
-static int _dummy_block_scan_remove(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_scan_remove(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "scan-remove");
 	return 0;
 }
 SID_UCMD_SCAN_REMOVE(_dummy_block_scan_remove)
 
-static int _dummy_block_error(struct module *module, struct sid_ucmd_ctx *ucmd_ctx)
+static int _dummy_block_error(sid_resource_t *mod_res, struct sid_ucmd_ctx *ucmd_ctx)
 {
 	log_debug(MID, "error");
 	return 0;
