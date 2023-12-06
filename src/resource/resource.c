95190bc0e1cd62647cb38d1f24c6aaba2f4eab75
Peter Rajnoha <prajnoha@redhat.com>
2023-12-06 15:31:26 +0100

    resource: differentiate resource-internal and resource-implementation logs on output
    Resource-internal logs now have "res-int" prefix and
    resource-implementation logs have "res-imp" prefix.

--- src/resource/resource.c
+++ src/resource/resource.c
@@ -1324,7 +1324,8 @@ static void _resource_log_output(sid_resource_t *res, log_req_t *log_req, const 
 	if (!res)
 		return;
 
-	req = (log_req_t) {.pfx = &((log_pfx_t) {.s = "res", .n = &((log_pfx_t) {.s = res->id, .n = NULL})}), .ctx = log_req->ctx};
+	req = (log_req_t) {.pfx = &((log_pfx_t) {.s = "res-int", .n = &((log_pfx_t) {.s = res->id, .n = NULL})}),
+	                   .ctx = log_req->ctx};
 
 	va_start(ap, fmt);
 	service_link_group_vnotify(res->slg, SERVICE_NOTIFICATION_MESSAGE, &req, fmt, ap);
@@ -1339,7 +1340,8 @@ void sid_resource_log_output(sid_resource_t *res, const log_req_t *log_req, cons
 	if (!res)
 		return;
 
-	req = (log_req_t) {.pfx = &((log_pfx_t) {.s = res->id, .n = log_req->pfx}), .ctx = log_req->ctx};
+	req = (log_req_t) {.pfx = &((log_pfx_t) {.s = "res-imp", .n = &((log_pfx_t) {.s = res->id, .n = log_req->pfx})}),
+	                   .ctx = log_req->ctx};
 
 	va_start(ap, fmt);
 	service_link_group_vnotify(res->slg, SERVICE_NOTIFICATION_MESSAGE, &req, fmt, ap);
