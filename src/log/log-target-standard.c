8fad32983c059929145fd8139a1d2549ac9a27e1
Peter Rajnoha <prajnoha@redhat.com>
2023-12-06 09:34:59 +0100

    log: standard: use new log_req_t instead of struct log_ctx for output fn

--- src/log/log-target-standard.c
+++ src/log/log-target-standard.c
@@ -59,28 +59,33 @@ void log_standard_close(void)
 	fflush(stderr);
 }
 
-void log_standard_output(const struct log_ctx *ctx, const char *format, va_list ap)
+void log_standard_output(const log_req_t *req, const char *format, va_list ap)
 {
 	FILE *out_file;
 
-	if (ctx->level_id > _max_level_id && ctx->level_id != LOG_PRINT)
+	if (req->ctx->level_id > _max_level_id && req->ctx->level_id != LOG_PRINT)
 		return;
 
-	out_file = _force_err_out ? stderr : ctx->level_id <= LOG_WARNING ? stderr : stdout;
+	out_file = _force_err_out ? stderr : req->ctx->level_id <= LOG_WARNING ? stderr : stdout;
 
 	if (_with_pids)
 		fprintf(out_file, "[%d:%d]:", getppid(), getpid());
 
 	if (_with_src_info)
-		fprintf(out_file, "%s:%d%s%s\t", ctx->src_file, ctx->src_line, ctx->src_func ? ":" : "", ctx->src_func ?: "");
+		fprintf(out_file,
+		        "%s:%d%s%s\t",
+		        req->ctx->src_file,
+		        req->ctx->src_line,
+		        req->ctx->src_func ? ":" : "",
+		        req->ctx->src_func ?: "");
 
-	if (ctx->prefix)
-		fprintf(out_file, "<%s> ", ctx->prefix);
+	if (req->pfx)
+		fprintf(out_file, "<%s> ", req->pfx->s);
 
 	vfprintf(out_file, format, ap);
 
-	if (ctx->errno_id)
-		fprintf(out_file, ": %s.", strerror(ctx->errno_id));
+	if (req->ctx->errno_id)
+		fprintf(out_file, ": %s.", strerror(req->ctx->errno_id));
 
 	fputc('\n', out_file);
 }
