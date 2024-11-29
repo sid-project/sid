4183f7f038667d7bdebb55a8c336093aff598d9b
Peter Rajnoha <prajnoha@redhat.com>
2024-11-29 12:43:03 +0100

    ci: update .csmock-kfp.js

--- .csmock-kfp.js
+++ .csmock-kfp.js
@@ -1,20 +1,19 @@
 {
     "scan": {
-        "analyzer-version-clang": "19.1.0",
+        "analyzer-version-clang": "19.1.4",
         "analyzer-version-cppcheck": "2.16.0",
         "analyzer-version-gcc": "14.2.1",
         "enabled-plugins": "clang, cppcheck, gcc",
         "exit-code": 0,
         "host": "fed.virt",
-        "known-false-positives": ".csmock-kfp.js",
         "mock-config": "default",
         "project-name": "sid-0.0.6-1.fc41",
-        "store-results-to": "/root/rpmbuild/SRPMS/sid-csmock-results.tar.xz",
-        "time-created": "2024-11-08 10:31:40",
-        "time-finished": "2024-11-08 10:34:18",
+        "store-results-to": "/root/rpmbuild/SRPMS/csmock-results",
+        "time-created": "2024-12-03 15:56:50",
+        "time-finished": "2024-12-03 15:59:12",
         "tool": "csmock",
-        "tool-args": "'/usr/bin/csmock' '--known-false-positives' '.csmock-kfp.js' '--cppcheck-add-flag=--check-level=exhaustive' '-o' 'sid-csmock-results.tar.xz' '-r' 'default' '--tools' 'clang,cppcheck,gcc' './sid-0.0.6-1.fc41.src.rpm'",
-        "tool-version": "csmock-3.7.1-1.fc41"
+        "tool-args": "'/usr/bin/csmock' '--cppcheck-add-flag=--check-level=exhaustive' '-r' 'default' '--tools' 'clang,cppcheck,gcc' 'sid-0.0.6-1.fc41.src.rpm' '-o' 'csmock-results'",
+        "tool-version": "csmock-3.8.0-1.fc41"
     },
     "defects": [
         {
@@ -34,7 +33,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 811,
+                    "line": 804,
                     "column": 6,
                     "event": "note",
                     "message": "Assuming 'args' is non-null",
@@ -42,7 +41,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 811,
+                    "line": 804,
                     "column": 2,
                     "event": "note",
                     "message": "Taking false branch",
@@ -50,7 +49,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 6,
                     "event": "note",
                     "message": "Assuming the condition is false",
@@ -58,7 +57,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 6,
                     "event": "note",
                     "message": "Left side of '||' is false",
@@ -66,7 +65,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 63,
                     "event": "note",
                     "message": "Assuming field 'key' is non-null",
@@ -82,7 +81,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 63,
                     "event": "note",
                     "message": "Left side of '||' is false",
@@ -98,7 +97,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 63,
                     "event": "note",
                     "message": "Assuming the condition is false",
@@ -122,7 +121,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 814,
+                    "line": 807,
                     "column": 2,
                     "event": "note",
                     "message": "Taking false branch",
@@ -130,7 +129,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 822,
+                    "line": 813,
                     "column": 2,
                     "event": "note",
                     "message": "Value assigned to 'c_archive_key'",
@@ -138,7 +137,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 826,
+                    "line": 817,
                     "column": 74,
                     "event": "note",
                     "message": "Assuming 'c_archive_key' is equal to NULL",
@@ -146,7 +145,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 829,
+                    "line": 820,
                     "column": 11,
                     "event": "note",
                     "message": "Calling '_unset_value'",
@@ -154,15 +153,15 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 788,
+                    "line": 781,
                     "column": 2,
                     "event": "note",
-                    "message": "Control jumps to 'case SID_KVS_BACKEND_HASH:'  at line 789",
+                    "message": "Control jumps to 'case SID_KVS_BACKEND_HASH:'  at line 782",
                     "verbosity_level": 2
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 790,
+                    "line": 783,
                     "column": 8,
                     "event": "note",
                     "message": "Value assigned to 'relay.archive_arg.has_archive', which participates in a condition later",
@@ -170,7 +169,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 790,
+                    "line": 783,
                     "column": 8,
                     "event": "note",
                     "message": "Value assigned to 'relay.archive_arg.kv_store_value', which participates in a condition later",
@@ -178,15 +177,15 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 791,
+                    "line": 784,
                     "column": 4,
                     "event": "note",
-                    "message": " Execution continues on line 798",
+                    "message": " Execution continues on line 791",
                     "verbosity_level": 2
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 798,
+                    "line": 791,
                     "column": 6,
                     "event": "note",
                     "message": "Assuming 'r' is >= 0",
@@ -194,7 +193,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 798,
+                    "line": 791,
                     "column": 6,
                     "event": "note",
                     "message": "Left side of '||' is false",
@@ -202,7 +201,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 798,
+                    "line": 791,
                     "column": 15,
                     "event": "note",
                     "message": "Assuming field 'ret_code' is >= 0",
@@ -210,7 +209,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 798,
+                    "line": 791,
                     "column": 2,
                     "event": "note",
                     "message": "Taking false branch",
@@ -218,7 +217,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 801,
+                    "line": 794,
                     "column": 2,
                     "event": "note",
                     "message": "Returning without writing to 'kv_store->backend', which participates in a condition later",
@@ -226,7 +225,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 829,
+                    "line": 820,
                     "column": 11,
                     "event": "note",
                     "message": "Returning from '_unset_value'",
@@ -234,7 +233,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 829,
+                    "line": 820,
                     "column": 2,
                     "event": "note",
                     "message": "Taking false branch",
@@ -242,7 +241,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 832,
+                    "line": 823,
                     "column": 6,
                     "event": "note",
                     "message": "Assuming field 'has_archive' is true",
@@ -250,7 +249,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 832,
+                    "line": 823,
                     "column": 6,
                     "event": "note",
                     "message": "Left side of '&&' is true",
@@ -258,7 +257,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 832,
+                    "line": 823,
                     "column": 39,
                     "event": "note",
                     "message": "Assuming field 'kv_store_value' is non-null",
@@ -266,7 +265,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 832,
+                    "line": 823,
                     "column": 2,
                     "event": "note",
                     "message": "Taking true branch",
@@ -274,7 +273,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 839,
+                    "line": 830,
                     "column": 23,
                     "event": "note",
                     "message": "Passing null pointer value via 2nd parameter 'key'",
@@ -282,7 +281,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/kvs.c",
-                    "line": 838,
+                    "line": 829,
                     "column": 12,
                     "event": "note",
                     "message": "Calling '_set_value'",
@@ -350,7 +349,7 @@
             "events": [
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/ubr.c",
-                    "line": 827,
+                    "line": 845,
                     "column": 55,
                     "event": "warning[-Warray-bounds=]",
                     "message": "array subscript 5 is outside array bounds of 'struct kv_vector_t[5]'",
@@ -360,7 +359,7 @@
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  827 |                 vvalue[VVALUE_IDX_DATA_ALIGNED + idx] = (kv_vector_t) {data, data_size};",
+                    "message": "  845 |                 vvalue[VVALUE_IDX_DATA_ALIGNED + idx] = (kv_vector_t) {data, data_size};",
                     "verbosity_level": 1
                 },
                 {
@@ -379,7 +378,7 @@
                 },
                 {
                     "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/ubr.c",
-                    "line": 6895,
+                    "line": 7310,
                     "column": 22,
                     "event": "note",
                     "message": "at offset 80 into object 'vvalue' of size 80",
@@ -389,7 +388,7 @@
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": " 6895 |         kv_vector_t  vvalue[VVALUE_SINGLE_CNT];",
+                    "message": " 7310 |         kv_vector_t  vvalue[VVALUE_SINGLE_CNT];",
                     "verbosity_level": 1
                 },
                 {
@@ -403,35 +402,130 @@
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  825|   \tif (VVALUE_FLAGS(vvalue) & SID_KV_FL_ALIGN) {",
+                    "message": "  843|   \tif (VVALUE_FLAGS(vvalue) & SID_KV_FL_ALIGN) {",
                     "verbosity_level": 1
                 },
                 {
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  826|   \t\tassert(vvalue_size >= VVALUE_IDX_DATA_ALIGNED + idx);",
+                    "message": "  844|   \t\tassert(vvalue_size >= VVALUE_IDX_DATA_ALIGNED + idx);",
                     "verbosity_level": 1
                 },
                 {
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  827|-> \t\tvvalue[VVALUE_IDX_DATA_ALIGNED + idx] = (kv_vector_t) {data, data_size};",
+                    "message": "  845|-> \t\tvvalue[VVALUE_IDX_DATA_ALIGNED + idx] = (kv_vector_t) {data, data_size};",
                     "verbosity_level": 1
                 },
                 {
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  828|   \t} else {",
+                    "message": "  846|   \t} else {",
                     "verbosity_level": 1
                 },
                 {
                     "file_name": "",
                     "line": 0,
                     "event": "#",
-                    "message": "  829|   \t\tassert(vvalue_size >= VVALUE_IDX_DATA + idx);",
+                    "message": "  847|   \t\tassert(vvalue_size >= VVALUE_IDX_DATA + idx);",
+                    "verbosity_level": 1
+                }
+            ]
+        },
+        {
+            "checker": "COMPILER_WARNING",
+            "language": "c/c++",
+            "tool": "gcc",
+            "hash_v1": "9319daeb237eae066205b57ff503fc7160e64785",
+            "key_event_idx": 1,
+            "events": [
+                {
+                    "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/ubr.c",
+                    "line": 0,
+                    "event": "scope_hint",
+                    "message": "In function '_do_sid_ucmd_dev_stack_get'",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/ubr.c",
+                    "line": 4023,
+                    "column": 39,
+                    "event": "warning[-Wuse-after-free]",
+                    "message": "pointer 'strv1_30' used after 'free'",
+                    "verbosity_level": 0
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4023 |                                 strv  = strv1;",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": "      |                                 ~~~~~~^~~~~~~",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "sid-0.0.6-build/sid-0.0.6/src/resource/ubr.c",
+                    "line": 4022,
+                    "column": 33,
+                    "event": "note",
+                    "message": "call to 'free' here",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4022 |                                 free(strv);",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": "      |                                 ^~~~~~~~~~",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4021|   \t\t\tif (strv1 != strv) {",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4022|   \t\t\t\tfree(strv);",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4023|-> \t\t\t\tstrv  = strv1;",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4024|   \t\t\t\tcount = count1;",
+                    "verbosity_level": 1
+                },
+                {
+                    "file_name": "",
+                    "line": 0,
+                    "event": "#",
+                    "message": " 4025|   \t\t\t}",
                     "verbosity_level": 1
                 }
             ]
